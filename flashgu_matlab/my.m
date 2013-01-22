edf290a4ab973cb013d552b2646032f32b0159bb
Duan Gu <guduanziliao@sina.com>
2013-01-23 00:18:25 +0100

    130123am

--- flashgu_matlab/my.m
+++ flashgu_matlab/my.m
@@ -1,17 +1,7 @@
 clc;
-a1=importdata([elegant_file_root 'flash_dfs10.mat1']);
-b1=a1.data;
-
- for i=1:nElement
-     for j=1:6
-         for k=1:6
-             Tmat1(j,k,i)=b1(i+1,(j-1)*6+k);
-         end
-     end
- end
- 
-Rmat=getTmatAll(Tmat1,33,44);
-Qmat=Tmat1(:,:,33);
-QR=(1-Qmat(1,1))*Rmat(1,1)-Qmat(2,1)*Rmat(1,2);
-QR2=getTmatAll(Tmat1,33,44)-getTmatAll(Tmat1,32,44);
-a=QR2(1,1)
+diffQR=QRmat1-QRmat2;
+difforbit=orbit_real1-orbit_real2;
+qoffset_calculated=pinv(diffQR)*difforbit;
+tt1=cell2mat({qoffset_calculated,qoffset_real});
+figure;
+bar(tt1);title('Quad-Offset comparison after 1st correction');
