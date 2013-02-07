93766b019613f95b30c2ba8623ce7dd87d46d5bb
guduan <guduanziliao@sina.com>
2013-02-07 17:43:23 +0100

    launch只带angle时正确了
    带入射位置时还是显示响应矩阵不是满秩

--- flashgu_matlab/getLRmat.m
+++ flashgu_matlab/getLRmat.m
@@ -12,16 +12,8 @@ nBpm=length(bpmid);
 nCorr=length(corrid);
 nQuad=length(quadid);
 LR=[];
-% for i=1:nBpm
-%     a=getTmatAll(Tmat,0,bpmid(i));
-%     b=getTmatAll(Tmat,1,bpmid(i));
-%     LR(i,1)=b(1,1)-Tmat(1,1,1)*b(1,1)-Tmat(2,1,1)*b(1,2); %R11 response to the incoming position
-%     LR(i,2)=b(1,2)-Tmat(1,1,1)*b(1,1)-Tmat(2,1,1)*b(1,2); %R12  response to the incoming angle
-% end
-
 for i=1:nBpm
-    a=getTmatAll(Tmat,0,bpmid(i));
-    LR(i,1)=a(1,1); %R11 response to the incoming position
-    LR(i,2)=a(1,2); %R12  response to the incoming angle
+    a=getTmatAll(Tmat,1,bpmid(i));
+    LR(i,1)=a(1,2); %R12  response to the incoming angle
 end
 
