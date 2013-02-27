78775017b5977b2b170cd730efcb297c2bb3c22e
guduan <guduanziliao@sina.com>
2013-02-27 17:54:23 +0100

    只用Undulator段时成功！

--- flashgu_matlab/my.m
+++ flashgu_matlab/my.m
@@ -23,8 +23,8 @@ LRmat=DFS_LRmat_Get(Tmat,status);
 
 % status.unuseQuadlist=[8 10 12 14 16 18 20 21];% Quadlist that NOT use
 % status.unuseBpmlist=[1 6 9 12 15 18 21];% Bpmlist that NOT use
-status.unuseQuadlist=[8 10 12 14 16];% Quadlist that NOT use
-status.unuseBpmlist=[1];% Bpmlist that NOT use
+status.unuseQuadlist=[2 4 6 8 10];% Quadlist that NOT use
+status.unuseBpmlist=[1 2];% Bpmlist that NOT use
 % status.unuseQuadlist=[];% Quadlist that NOT use
 % status.unuseBpmlist=[];% Bpmlist that NOT use
 status=modify_list(status);
